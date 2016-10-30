97c24fc7b5dcfd837e77638f9eaac5b1b00b0411
Shawn Huang <shawnhuang@ShawndeMacBook-Pro.local>
2016-10-30 09:13:57 +0800

    Initial Commit

---
+++ monster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  monster
+//
+//  Created by Shawn Huang on 2016/10/30.
+//  Copyright © 2016年 Shawn Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
